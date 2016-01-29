304a5f8ce65805e1d0b03dd64b54eb21641eed28
150206 <150206@nhtv.nl>
2016-01-29 21:15:51 +0100

    [Add] - Raycast for block pickup.

--- Prototype/GGJ Prototype/Assets/Player.cs
+++ Prototype/GGJ Prototype/Assets/Player.cs
@@ -5,18 +5,23 @@ public class Player : MonoBehaviour {
 
     public int m_Player;
     public float m_XSpeed;
-    public float m_YSpeed;
+    public float m_ZSpeed;
+    public float m_PickupOffset;
 
     CharacterController m_CharacterController;
+    Vector3 m_FaceDirection;
     Vector3 m_MoveDirection;
     string m_HorizontalInput = "Horizontal";
     string m_VerticalInput = "Vertical";
+    string m_PickupInput = "Pickup";
     // Use this for initialization
     void Awake ()
     {
         m_CharacterController = GetComponent<CharacterController>();
         m_HorizontalInput += m_Player.ToString();
         m_VerticalInput += m_Player.ToString();
+        m_PickupInput += m_Player.ToString();
+        m_FaceDirection = Vector3.forward;
         m_MoveDirection = Vector3.zero;
     }
 	
@@ -27,17 +32,26 @@ public class Player : MonoBehaviour {
         {
             m_MoveDirection = new Vector3(Input.GetAxis(m_HorizontalInput)* m_XSpeed, 0, 0);
         }
-        else //if(Mathf.Abs(Input.GetAxis(m_HorizontalInput)) <= Mathf.Abs(Input.GetAxis(m_VerticalInput)))
+        else
         {
-            m_MoveDirection = new Vector3(0, 0, Input.GetAxis(m_VerticalInput)* m_YSpeed);
+            m_MoveDirection = new Vector3(0, 0, Input.GetAxis(m_VerticalInput)* m_ZSpeed);
         }
 
-	}
+        // Block pickup.
+        Vector3 direction = m_FaceDirection.normalized * m_PickupOffset;
+        Vector3 position = new Vector3(transform.position.x + direction.x, transform.position.y, transform.position.z + direction.z);
+        RaycastHit hit;
+        if(Physics.Raycast(position, Vector3.down, out hit, 1f))
+        {
+            hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
 
     void FixedUpdate()
     {
         if(m_MoveDirection.x != 0 || m_MoveDirection.z != 0)
         {
+            m_FaceDirection = m_MoveDirection.normalized;
             transform.rotation = Quaternion.LookRotation(m_MoveDirection.normalized);
         }
         m_CharacterController.Move(m_MoveDirection);
