d502d5e878740412208518c245b7b58cdef15c4a
WarriorMario <marioleiden@live.nl>
2016-01-29 19:25:05 +0100

    [Add] - Initial project commit

---
+++ Prototype/GGJ Prototype/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    CharacterController m_CharacterController;
+	// Use this for initialization
+	void Awake ()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
