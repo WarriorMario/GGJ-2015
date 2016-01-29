ad981f5f7dd8bcc8b33a01ba2f6e1197ee09e131
WarriorMario <marioleiden@live.nl>
2016-01-29 20:09:40 +0100

    [Add] - New material for the block direction [Change] - Movement is 4way

--- Prototype/GGJ Prototype/Assets/Player.cs
+++ Prototype/GGJ Prototype/Assets/Player.cs
@@ -3,18 +3,39 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    public int m_Player;
+    public float m_XSpeed;
+    public float m_YSpeed;
+
     CharacterController m_CharacterController;
     Vector3 m_MoveDirection;
-	// Use this for initialization
-	void Awake ()
+    string m_HorizontalInput = "Horizontal";
+    string m_VerticalInput = "Vertical";
+    // Use this for initialization
+    void Awake ()
     {
         m_CharacterController = GetComponent<CharacterController>();
-	}
+        m_HorizontalInput += m_Player.ToString();
+        m_VerticalInput += m_Player.ToString();
+        m_MoveDirection = Vector3.zero;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        m_MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        m_CharacterController.Move(m_MoveDirection);
+        if (Mathf.Abs(Input.GetAxis(m_HorizontalInput)) > Mathf.Abs(Input.GetAxis(m_VerticalInput)))
+        {
+            m_MoveDirection = new Vector3(Input.GetAxis(m_HorizontalInput)* m_XSpeed, 0, 0);
+        }
+        else //if(Mathf.Abs(Input.GetAxis(m_HorizontalInput)) <= Mathf.Abs(Input.GetAxis(m_VerticalInput)))
+        {
+            m_MoveDirection = new Vector3(0, 0, Input.GetAxis(m_VerticalInput)* m_YSpeed);
+        }
 	}
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(m_MoveDirection.normalized);
+        m_CharacterController.Move(m_MoveDirection);
+    }
 }
